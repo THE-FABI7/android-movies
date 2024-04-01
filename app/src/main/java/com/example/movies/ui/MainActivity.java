fa55c09d85d045d3d4059ef21ffd77f883aa3059
THE FABI7 <fabianguancha13@gmail.com>
2024-03-31 22:08:34 -0500

    modificacion de la seguridad de la red

--- app/src/main/java/com/example/movies/ui/MainActivity.java
+++ app/src/main/java/com/example/movies/ui/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.icu.text.IDNA;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -36,6 +37,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new
+                    StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
         initViews();
         initEvents();
     }
